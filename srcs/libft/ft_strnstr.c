34cb1faa8e9d42c156c2dba1f84b1129691ed0f1
SirMengo <msimoes0103@gmail.com>
2025-06-09 12:21:03 +0100

    Almost done

--- srcs/libft/ft_strnstr.c
+++ srcs/libft/ft_strnstr.c
@@ -1,26 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/20 11:58:34 by msimoes           #+#    #+#             */
-/*   Updated: 2025/06/06 12:02:41 by msimoes          ###   ########.fr       */
+/*   Created: 2025/04/14 17:58:32 by msimoes           #+#    #+#             */
+/*   Updated: 2025/06/06 12:06:59 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
-# include <stdio.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdlib.h>
+#include "libft.h"
 
-# include "srcs/ft_printf/ft_printf.h"
-# include "srcs/libft/libft.h"
+char	*ft_strnstr(const char *bg, const char *ltl, size_t len)
+{
+	size_t	i;
+	size_t	j;
 
-void	error(void);
-void	cmd_exec(char argv, char *envp[]);
-
-#endif
+	if (*ltl == '\0')
+		return ((char *)(bg));
+	i = 0;
+	while (i < len && bg[i] != '\0')
+	{
+		j = 0;
+		if (bg[i] == ltl[0])
+		{
+			while ((i + j < len) && ltl[j] != '\0' && bg[i + j] == ltl[j])
+				j++;
+			if (ltl[j] == '\0')
+				return ((char *)(bg + i));
+		}
+		i++;
+	}
+	return (0);
+}
--- utils.c
+++ utils.c
@@ -6,15 +6,64 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 11:38:17 by msimoes           #+#    #+#             */
-/*   Updated: 2025/06/04 15:52:57 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/06/09 12:15:20 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+char	*find_path(char	*envp[], char *cmd)
+{
+	int		i;
+	char	**paths;
+	char	*path;
+	char	*add_path;
+
+	i = 0;
+	while(ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while(paths[i])
+	{
+		add_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(add_path,cmd);
+		free(add_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = -1;
+	while (paths[i])
+	{
+		i++;
+		free(paths[i]);
+	}
+	return(0);
+}
+
 void	cmd_exec(char argv, char *envp[])
 {
-	execve("/bin/bash", argv, NULL);
+	char	*path;
+	char	**cmd;
+	int		i;
+
+	i = -1;
+	cmd = ft_split(argv, ' ');
+	path = find_path(envp, cmd[0]);
+	if (!path)
+	{
+		while(cmd[i])
+		{
+			i++;
+			free(cmd[i]);
+		}
+		free(cmd);
+		error();
+	}
+	if (execve(path, cmd, envp) == -1)
+		error();
 }
 
 void	error()
